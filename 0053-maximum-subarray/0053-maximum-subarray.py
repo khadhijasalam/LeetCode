d388384f548925a2834e9822c84f782112cfbe5c
Khadhija <165560176+khadhijasalam@users.noreply.github.com>
2025-07-26 11:40:56 +0530

    Time: 63 ms (63.93%), Space: 21.2 MB (63.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,6 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+#     Kadane’s Algorithm is the optimal solution for finding the maximum subarray sum in an array of integers.
+
+# ✅ Time Complexity: O(n) (single pass through the array)
+# ✅ Space Complexity: O(1) (uses only a few variables)
  
         curr=0
         maxi=float('-inf')
