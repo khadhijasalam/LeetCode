be471dd3d309af0df748400d5126144899b83d03
Khadhija <165560176+khadhijasalam@users.noreply.github.com>
2025-07-27 20:13:32 +0530

    Time: 4 ms (5.91%), Space: 12.4 MB (84.64%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,60 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i = n - 2
+        
+        # 1. Find first decreasing element from right
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        
+        if i >= 0:
+            # 2. Find element just bigger than nums[i] on right
+            j = n - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        # 3. Reverse the part after i
+        # left, right = i + 1, n - 1
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 
+        nums[i + 1:] = sorted(nums[i + 1:])
+
+
+
+
+
+
+
+
+
+
+
+#############Not completely right:
+        # left=0
+        # right=len(nums)-1
+        # ptr=len(nums)-2
+        # temp=[]
+        # while left<=right and ptr>=0:
+        #     if nums[ptr]<nums[ptr+1]:
+        #         left=ptr
+        #         nums[ptr],nums[right]= nums[right],nums[ptr]
+        #         temp=sorted(nums[left+1:])
+        #         # print("temp",temp)
+        #         for i in range(len(temp)):
+        #             nums[left+1]=temp[i]
+        #             left+=1
+        #             # print(nums[i])
+        #         # print("hello",nums)
+
+        #         return nums          
+        #     ptr-=1
+        # # print(sorted(nums))
+        # return nums.sort()
+  
