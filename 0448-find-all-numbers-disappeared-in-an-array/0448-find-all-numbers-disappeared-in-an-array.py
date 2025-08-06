86a419c32f64f18739e0aa5474fe930f5ba8f3c3
Khadhija <165560176+khadhijasalam@users.noreply.github.com>
2025-08-06 20:49:25 +0530

    Time: 63 ms (18.54%), Space: 29.3 MB (26.34%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -4,10 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        s=set(nums)
-        l=len(nums)
-        ret=[]
-        for i in range(1,l+1):
-            if i not in s:
-                ret.append(i)
-        return ret
+
+      
+    # First pass: Mark seen numbers by negating the value at the corresponding index
+        for i in range(len(nums)):
+            index = abs(nums[i]) - 1  # Convert value to index (1-based to 0-based)
+            if nums[index] > 0:
+                nums[index] *= -1     # Mark as seen by making it negative
+
+        # Second pass: Collect indices that remain positive (i.e., numbers that were not seen)
+        result = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i + 1)  # i+1 is missing since nums[i] is still positive
+
+        return result
+
+
+
+        
+        # s=set(nums)
+        # l=len(nums)
+        # ret=[]
+        # for i in range(1,l+1):
+        #     if i not in s:
+        #         ret.append(i)
+        # return ret
