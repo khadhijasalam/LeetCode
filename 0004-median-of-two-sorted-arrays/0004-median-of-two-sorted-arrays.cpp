520b17936d4e3451d572f472aad7e6e393029010
Khadhija <165560176+khadhijasalam@users.noreply.github.com>
2025-08-15 01:18:53 +0530

    Time: 51 ms (5.4%), Space: 97.1 MB (10.32%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        double answer;
+        int sizeNums1=nums1.size();
+        int sizeNums2=nums2.size();
+        vector<int> numsM;
+        vector<int> numsMS;
+        int sizeNumsMS;
+        numsM.insert(numsM.end(),nums1.begin(),nums1.end());
+numsM.insert(numsM.end(),nums2.begin(),nums2.end());
+        int sizeNumsM=numsM.size();
+        int temp;
+        int tempi;
+        int k;
+        int min;
+        for (int i=0;i<=sizeNumsM;i++)
+            {
+                min=*min_element(numsM.begin(),numsM.end());
+                numsMS.push_back(min);
+                vector<int> :: iterator itr;
+                itr = find(numsM.begin(), numsM.end(),min);
+                if (itr!=numsM.end())
+                {
+                    tempi = distance(numsM.begin(), itr);
+                }
+                numsM.erase(numsM.begin()+tempi);
+            }
+        sizeNumsMS=numsMS.size();
+        double med1;
+        double med2;
+        if (sizeNumsMS%2!=0) //even
+        {
+            med1=numsMS[(sizeNumsMS)/2];
+            med2=numsMS[(sizeNumsMS/2)-1];
+            answer=(med1+med2)/2;
+        }
+        else //odd
+        {
+            med1=numsMS[(sizeNumsMS-1)/2];
+            answer=med1;
+        }
+        return answer;
+        
+    }
+};
+        
