739395c18a149455b5d14ea236305a6c3999c4aa
Khadhija <165560176+khadhijasalam@users.noreply.github.com>
2025-08-23 15:31:12 +0530

    Time: 0 ms (100%), Space: 17.8 MB (33.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,35 +4,62 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         l=len(nums)
-        # for i in range(l-1):
+        # Optimal
+        left=0
+        mid=0
+        right=l-1
+        while mid<=right:
+            if nums[mid]==0:
+                nums[left],nums[mid]=nums[mid],nums[left]
+                mid+=1
+                left+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[right]=nums[right],nums[mid]
+                right-=1
+        return nums
+            
+            
+
+
+
+
+        #Better
+   
+        # count=[0,0,0]
+        # for i in nums:
+        #     if i==0:
+        #         count[0]+=1
+        #     elif i ==1:
+        #         count[1]+=1
+        #     else:
+        #         count[2]+=1
+        # print(count)
+        # c_red=0
+        # c_white=0
+        # # c_blue=
+        # for i in range(l):
+        #     if c_red != count[0]:
+        #         nums[i]=0
+        #         c_red+=1
+                
+        #     elif c_white!= count[1]:
+        #         nums[i]=1
+        #         c_white+=1
+              
+        #     else:
+        #         nums[i]=2
+        # return nums
+
+
+
+        #Brute
+             # for i in range(l-1):
         #     for j in range(i+1, l):
         #         if nums[i]>nums[j]:
         #             nums[i],nums[j]= nums[j], nums[i]
         # return nums
-        count=[0,0,0]
-        for i in nums:
-            if i==0:
-                count[0]+=1
-            elif i ==1:
-                count[1]+=1
-            else:
-                count[2]+=1
-        print(count)
-        c_red=0
-        c_white=0
-        # c_blue=
-        for i in range(l):
-            if c_red != count[0]:
-                nums[i]=0
-                c_red+=1
-                
-            elif c_white!= count[1]:
-                nums[i]=1
-                c_white+=1
-              
-            else:
-                nums[i]=2
-        return nums
             
             
             
