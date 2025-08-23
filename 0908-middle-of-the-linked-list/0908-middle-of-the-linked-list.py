c261f2cc9c8069519675f27be75dba3343aa7ca1
Khadhija <165560176+khadhijasalam@users.noreply.github.com>
2025-08-24 00:47:22 +0530

    Time: 0 ms (100%), Space: 17.7 MB (48.11%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -5,27 +5,41 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr=head
-        count=0
-        while curr is not None:
-            # print(curr.val)
-            count+=1
-            curr=curr.next
-            # print(count)
-        if count%2==0:
-            mid= int((count/2 )+1)
-        else: 
-            mid=ceil(count/2)
-        # print(mid)
-        c=0
-        curr=head
-        # ans=[]
-        while curr is not None:
-            # print(curr.val)
-            c+=1
-            if c==mid:
-                # ans.append(curr.val)
-                return curr
+        # curr=head
+        # count=0
+        # while curr is not None:
+        #     # print(curr.val)
+        #     count+=1
+        #     curr=curr.next
+        #     # print(count)
+        # if count%2==0:
+        #     mid= int((count/2 )+1)
+        # else: 
+        #     mid=ceil(count/2)
+        # # print(mid)
+        # c=0
+        # curr=head
+        # # ans=[]
+        # while curr is not None:
+        #     # print(curr.val)
+        #     c+=1
+        #     if c==mid:
+        #         # ans.append(curr.val)
+        #         return curr
             
-            curr=curr.next
-        # return ans
+        #     curr=curr.next
+
+
+        #OPTIMAL:When fast reaches the end, slow will be at the middle. because fast is twice as fastas slow
+
+        slow = head
+        fast = head
+        
+        # Move fast 2x as quickly as slow
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # When fast reaches end, slow is in middle
+        return slow
+     
