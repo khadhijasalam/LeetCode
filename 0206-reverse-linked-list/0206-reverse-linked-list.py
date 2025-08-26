8b5e7300fc28bc47c2f477fee41b0ffe03f236fe
Khadhija <165560176+khadhijasalam@users.noreply.github.com>
2025-08-26 18:39:58 +0530

    Time: 1013 ms (0.74%), Space: 23.4 MB (5.16%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -7,19 +7,44 @@ from collections import deque
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack=deque()
-        curr=head
-        while curr is not None:
-            stack.append(curr.val)
-            
-            curr=curr.next
-        curr=head
-        while stack:
-            curr.val=stack.pop()
-            curr=curr.next
-            # print(curr)
 
-        return head
+        prev=None
+        curr=head
+        print(curr)
+       
+        while curr:
+            next_node=curr.next
+            
+            curr.next=prev
+            prev=curr
+            curr=next_node
+        # curr.next=None
+        print(prev)
+        return prev
+
+
+
+
+
+
+        # stack=deque()
+        # curr=head
+        # while curr is not None:
+        #     stack.append(curr.val)
+            
+        #     curr=curr.next
+        # curr=head
+        # while stack:
+        #     curr.val=stack.pop()
+        #     curr=curr.next
+        #     # print(curr)
+
+        # return head
+
+        
+
+      
+    
 
 
        
